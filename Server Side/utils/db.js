1001399134a7d339eee92091a47f932936266884
Okidoki <shahzebakhtar66@gmail.com>
2024-04-21 21:04:12 +0530

    added db url

--- Server Side/utils/db.js
+++ Server Side/utils/db.js
@@ -1,13 +1,22 @@
-import mysql from 'mysql'
+import pg from 'pg';
 
-const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "employeems"
-})
+const { Client } = pg;
 
-con.connect(function(err) {
+// const con = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database: "employeems"
+// })
+const connectionString = "postgresql://shahzebakhtar66:z7v6bFPRiafu@ep-morning-violet-a5txgtbx.us-east-2.aws.neon.tech/prisma_migrate_shadow_db_13c76688-1fee-4e73-8528-e144b56639fd?sslmode=require";
+
+const con = new Client({
+    connectionString: connectionString,
+    ssl: {
+      rejectUnauthorized: false // Necessary if you're using self-signed SSL certificates
+    }
+  });
+  con.connect(function(err) {
     if(err) {
         console.log("connection error")
     } else {
@@ -15,5 +24,5 @@ con.connect(function(err) {
     }
 })
 
-export default con;
+export default {con};
 
